000a8c6ed85f210246acfcff082e0d2f496a6536
SerkanYalçınkaya <syalcinkaya895@gmail.com>
2024-10-06 13:46:58 +0300

    feat: added sitemap and robots file

---
+++ app/sitemap.ts
@@ -0,0 +1,42 @@
+import { writingsTR } from '@/constants'
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+
+  const writings = writingsTR.map((item)=>({
+    url:`https://serkanyalcinkaya.vercel.app${item.link}`,
+    lastModified:new Date(),
+    changeFrequency: "monthly",
+    priority:0.6
+  }))
+  return [
+    {
+      url: 'https://serkanyalcinkaya.vercel.app/',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://serkanyalcinkaya.vercel.app/wrintgs',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://serkanyalcinkaya.vercel.app/codes',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://serkanyalcinkaya.vercel.app/projects',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.5,
+    },
+    ...writings.map(writing => ({
+        ...writing,
+        changeFrequency: writing.changeFrequency as "monthly" | "yearly" | "always" | "hourly" | "daily" | "weekly" | "never",
+    }))
+  ]
+}
